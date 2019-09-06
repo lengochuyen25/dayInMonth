e554618bdcca067a26effc30fcab8b0d9580cb25
lengochuyen25 <huyenln.hvnh@gmail.com>
2019-09-06 12:02:57 +0700

    a

---
+++ src/main/java/dayOfMonth.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class dayOfMonth {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Which month that you want to count day? ");
+        int month = scanner.nextInt();
+        String dayInMonth;
+        switch (month) {
+            case 2:
+                dayInMonth = "28 or 29";
+                break;
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+            case 12:
+                dayInMonth = "31";
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                dayInMonth = "30";
+                break;
+            default:
+                dayInMonth = "";
+        }
+        if (dayInMonth != "") {
+            System.out.println("The month" + month + "has " + dayInMonth + " days!");
+        }
+        else {
+            System.out.print("Invalid input!");
+        }
+    }
+}
